559f207e6311353c9608e36b56c09423b4164a7b
Shawn Lauzon <shawn.lauzon@gmail.com>
2023-10-15 21:09:24 -0500

    Disable Cypress for now

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('cypress')
+// const { defineConfig } = require('cypress')
 
-module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:8888/",
-    supportFile: false,
-    chromeWebSecurity: false,
-  },
-});
+// module.exports = defineConfig({
+//   e2e: {
+//     baseUrl: "http://localhost:8888/",
+//     supportFile: false,
+//     chromeWebSecurity: false,
+//   },
+// });
