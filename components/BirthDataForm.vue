cd7e2d28d75079b6286d926dda30e39c072a445e
Shawn Lauzon <shawn.lauzon@gmail.com>
2023-10-17 09:34:25 -0500

    Make button full width

--- components/BirthDataForm.vue
+++ components/BirthDataForm.vue
@@ -33,7 +33,7 @@
     <v-row>
       <v-text-field v-model="city"></v-text-field>
     </v-row>
-    <v-btn :loading="isLoading" :disabled="isLoading" @click="handleNext"
+    <v-btn block :loading="isLoading" :disabled="isLoading" @click="handleNext"
       >Next</v-btn
     >
   </v-container>
@@ -565,36 +565,3 @@ export default {
   },
 }
 </script>
-
-<style scoped>
-.birth-data-form {
-  margin: 40px 40px;
-  min-width: 250px;
-  width: 45%;
-}
-input,
-textarea,
-button {
-  font-size: inherit;
-  margin: 15px 0;
-  padding: 12px 20px;
-  width: 100%;
-}
-
-button {
-  background-color: teal;
-  border: 1px solid black;
-  color: white;
-  width: 50%;
-}
-
-.hidden {
-  clip: rect(0 0 0 0);
-  clip-path: inset(50%);
-  height: 1px;
-  overflow: hidden;
-  position: absolute;
-  white-space: nowrap;
-  width: 1px;
-}
-</style>
