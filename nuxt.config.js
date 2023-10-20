9d382f456431c7e526622271944b39de03e088b3
Shawn Lauzon <shawn.lauzon@gmail.com>
2023-10-20 17:37:22 -0500

    Fix title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-toolbox',
+    title: 'Should I leave my job?',
     htmlAttrs: {
       lang: 'en',
     },
