a80ea0909cc0897fa5aaa79ab3784555c6d6393c
Shawn Lauzon <shawn.lauzon@gmail.com>
2023-10-22 17:58:00 -0500

    Enable store-chart

--- netlify/functions/store-chart.mjs
+++ netlify/functions/store-chart.mjs
@@ -1,48 +1,38 @@
-// import type { Handler } from '@netlify/functions'
-// import { withPlanetscale } from '@netlify/planetscale'
+import { withPlanetscale } from '@netlify/planetscale'
 
-// export const handler: Handler = withPlanetscale(async (event, context) => {
-//   const {
-//     planetscale: { connection },
-//   } = context
+export default withPlanetscale(async (request, context) => {
+  const {
+    planetscale: { connection },
+  } = context
 
-//   const { body } = event
+  const {
+    type,
+    profile,
+    cross,
+    definition,
+    innerAuthority,
+    birthDateLocal,
+    birthPlace,
+    traits,
+  } = await request.json()
 
-//   if (!body) {
-//     return {
-//       statusCode: 400,
-//       body: 'Missing body',
-//     }
-//   }
+  const result = await connection.execute(
+    `INSERT INTO charts (career_type, public_role, life_work, assimilation, decision_making_strategy,\
+       birth_date_local, birth_place, traits) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+    [
+      type,
+      profile,
+      cross,
+      definition,
+      innerAuthority,
+      birthDateLocal,
+      birthPlace,
+      traits,
+    ]
+  )
+  console.log('INSERT result', result)
 
-//   const {
-//     type,
-//     profile,
-//     cross,
-//     definition,
-//     innerAuthority,
-//     birthDateLocal,
-//     birthPlace,
-//     traits,
-//   } = JSON.parse(body)
-
-//   const result = await connection.execute(
-//     `INSERT INTO charts (career_type, public_role, life_work, assimilation, decision_making_strategy,\
-//        birth_date_local, birth_place, traits) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-//     [
-//       type,
-//       profile,
-//       cross,
-//       definition,
-//       innerAuthority,
-//       birthDateLocal,
-//       birthPlace,
-//       traits,
-//     ]
-//   )
-//   console.log('INSERT result', result)
-
-//   return {
-//     statusCode: 201,
-//   }
-// })
+  return {
+    statusCode: 201,
+  }
+})
