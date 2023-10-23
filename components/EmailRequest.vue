6f0e5318fd58b5e6abaadca42ca7a4c253c7d506
Shawn Lauzon <shawn.lauzon@gmail.com>
2023-10-23 11:37:34 -0500

    Fix undefined .required

--- components/EmailRequest.vue
+++ components/EmailRequest.vue
@@ -34,6 +34,18 @@ export default {
   props: {
     value: { type: String, default: null },
   },
+  data() {
+    return {
+      rules: {
+        required: (value) => !!value || 'Required.',
+        email: (value) => {
+          const pattern =
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          return pattern.test(value) || 'Invalid e-mail.'
+        },
+      },
+    }
+  },
   emit: ['agreed'],
 }
 </script>
