fe67880dc17df1d4a184bb803f98b32dd7672d8f
Shawn Lauzon <shawn.lauzon@gmail.com>
2023-10-24 11:35:25 -0500

    Better spacing on email form

--- components/EmailRequest.vue
+++ components/EmailRequest.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="text-h4 text-md-h2 elevation-0 d-flex flex-column">
+  <v-card class="text-h4 text-md-h2 mx-2 elevation-0 d-flex flex-column">
     <div class="pb-4">One last thing.</div>
     <div class="text-body-1 pb-6">
       This quiz is only a start. We'd like to continue working with you to
@@ -11,33 +11,39 @@
       info which I think you'll like. Most of it specifically for you and your
       unique design. Cancel anytime.
     </div>
-    <v-text-field
-      v-model="user.firstName"
-      label="First name"
-      :rules="[rules.required]"
-      @input="$emit('user', user)"
-    ></v-text-field>
-    <v-text-field
-      v-model="user.lastName"
-      label="Last name"
-      @input="$emit('user', user)"
-    ></v-text-field>
-    <v-text-field
-      v-model="user.email"
-      label="Email address"
-      :rules="[rules.required, rules.email]"
-      :error-messages="errors"
-      @input="$emit('user', user)"
-    ></v-text-field>
-    <v-checkbox
-      v-model="isAgreed"
-      :false-value="false"
-      :true-value="true"
-      :rules="[rules.required]"
-      row
-      label="I agree to receive email. Your email address will not be shared or sold. Pinkie swear!"
-      @change="$emit('agreed', $event)"
-    />
+    <div>
+      <v-row>
+        <v-text-field
+          v-model="user.firstName"
+          class="mx-2"
+          label="First name"
+          :rules="[rules.required]"
+          @input="$emit('user', user)"
+        ></v-text-field>
+        <v-text-field
+          v-model="user.lastName"
+          class="ml-2"
+          label="Last name"
+          @input="$emit('user', user)"
+        ></v-text-field>
+      </v-row>
+      <v-text-field
+        v-model="user.email"
+        label="Email address"
+        :rules="[rules.required, rules.email]"
+        :error-messages="errors"
+        @input="$emit('user', user)"
+      ></v-text-field>
+      <v-checkbox
+        v-model="isAgreed"
+        :false-value="false"
+        :true-value="true"
+        :rules="[rules.required]"
+        row
+        label="I agree to receive email. Your email address will not be shared or sold. Pinkie swear!"
+        @change="$emit('agreed', $event)"
+      />
+    </div>
   </v-card>
 </template>
 
