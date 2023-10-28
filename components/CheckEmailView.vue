63caedd1e4d0799b4704595fa246af1dbd64d840
Shawn Lauzon <shawn.lauzon@gmail.com>
2023-10-28 18:41:10 -0500

    Accidentally removed margin

--- components/CheckEmailView.vue
+++ components/CheckEmailView.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container class="container">
+  <v-container class="no-margins">
     <v-img class="hero" src="/winner.jpg" />
     <div class="text-h2 ma-4">Nice work!</div>
     <div class="text-body-1 mx-8">
@@ -9,7 +9,7 @@
 </template>
 
 <style>
-.container {
+.no-margins {
   padding: 0;
   margin: 0;
   max-width: none;
